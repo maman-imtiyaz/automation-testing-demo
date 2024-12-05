c29418c312d24bb38553863e6b556eac7184d2b0
maman-imtiyaz <man2.bungsu06@gmail.com>
2024-12-05 16:39:34 +0700

    Initial commit

---
+++ Object Repository/Login/btn_Login.rs
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Login</name>
+   <tag></tag>
+   <elementGuidId>5dc0159c-c8f6-4b48-9d1b-f85ede49d4d7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = '
+Sign In' or . = '
+Sign In')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+Sign In</value>
+   </webElementProperties>
+</WebElementEntity>
